80f1204945611a962e673a7023c0fbd15c78d0e6
Myhrindra194 <myhrindra194@gmail.com>
2023-10-25 17:34:17 +0300

    Bubblesort optimized

--- fonction.c
+++ fonction.c
@@ -9,16 +9,19 @@ void printArray(int array [],int size){
 
 // bubble sort ascending order
 void bubbleSort(int array [], int size){
-    int tmp = 0;
-    for(int i = 0; i < size; i++){
-        for(int j = 0; j < size-i-1; j++){
-            if(array[j] > array[j+1]){
-                tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1]= tmp;
+    int permutations = 1;
+
+        while (permutations > 0) {
+            permutations = 0;
+            for (int i = 0; i < size - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                    permutations++;
+                }
             }
         }
-    }
 }
 
 
